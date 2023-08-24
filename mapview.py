5ec7234668d6cd10216f5167c11cccb1f85bf222
dunland <david.unland@web.de>
2023-08-24 13:07:10 +0200

    refactor ROI/GIS class

--- mapview.py
+++ mapview.py
@@ -18,46 +18,76 @@ from kivy.uix.boxlayout import BoxLayout
 print("window size initial", Window.size)
 from kivy.graphics import Color
 
-class ROI:
-    def __init__(self):
-        self.lat_max = 54.1962362 # 7207409  # northernmost latitude
-        self.lat_min = 54.1884733 # 7205932  # southernmost latitude
-        self.lat_diff = self.lat_max - self.lat_min
-        self.lon_max = 9.1091775 # 1014029  # easternmost longitude
-        self.lon_min = 9.0995835 # 1012961  # westernmost longitude
-        self.lon_diff = self.lon_max - self.lon_min
+class GIS:
+    def __init__(self, coords=None):
+
+        self.gdf = geopandas.read_file("/home/dunland/github/qScope/data/GIS/Shapefiles/bestandsgebaeude_export.shp")#.to_crs(epsg=3857)
+
+        # if no coords provided, take polygons max extents as frame
+        if coords is None:
+            self.coords = {
+                'lon_min' : self.gdf.bounds['minx'].min(), # 9.1091775 # 1014029  # easternmost longitude
+                'lon_max' : self.gdf.bounds['maxx'].max(), # 9.0995835 # 1012961  # westernmost longitude
+                'lat_min' : self.gdf.bounds['miny'].min(), # 54.1962362 # 7207409  # northermost latitude
+                'lat_max' : self.gdf.bounds['maxy'].max() # 54.1884733 # 7205932  # southernmost latitude,
+            }
+
+        else:
+            self.coords = {
+                'lon_min' : coords[0],
+                'lon_max' : coords[1],
+                'lat_min' : coords[2],
+                'lat_max' : coords[3]
+            }
+
+        print("initialized GIS class with coords:", self.coords)
+
+
+    def set_bbox(self, coords):
+        self.coords = {
+            'lat_min' : coords[0],
+            'lon_min' : coords[1],
+            'lat_max' : coords[2],
+            'lon_max' : coords[3]
+        }
+
+        print("updated GIS bounding box with coords:", self.coords)
+
+    def coords_to_point(self, lon, lat):
+        #longtitude 9 something
+        x = (lon - self.coords['lon_min']) * Window.size[0] / (self.coords['lon_max'] - self.coords['lon_min'])
+
+        #latitude 54 something
+        y = (lat - self.coords['lat_min']) * Window.size[1] / (self.coords['lat_max'] - self.coords['lat_min'])
+
+        print("translated points to", x, y)
+        return x, y
 
 class MapViewApp(App):
-    def coords_to_point(self, coord, wid):
-        #longtitude 9 something
-        x = (coord[0] - roi.lon_min) * wid.size[0] / roi.lon_diff
-        
-        #latitude 54 something
-        y = (coord[1] - roi.lat_min) * wid.size[1] / roi.lat_diff
-        
-        return x, y
-    def build_mesh(self, wid, coords):
+
+    def build_mesh(self, coords):
         vertices = []
         # vertices.extend([720, 480, 0, 0]) # shows where is the center of the widget
         indices = []
 
         for i, coord in enumerate(coords):
-            x, y = self.coords_to_point(coord, wid)
+            x, y = gis.coords_to_point(coord[0], coord[1])
             vertices.extend([x, y, 0, 0])
             indices.append(i)
         return Mesh(vertices=vertices, indices=indices)
-        
+
     def build(self):
-        mapview = MapView(zoom=16, lat=54.19216979440788, lon=9.105268718909326)
+        mapview = MapView(zoom=12, lat=54.19216979440788, lon=9.105268718909326)
 
         wid = Widget(pos=(0, 0), size=Window.size)
         wid.canvas.add(Color(0, 0, 0))
         # print("==== widget size", wid.size)
-        gdf = geopandas.read_file("bestandsgebaeude_export.shp")
+
+        gis.set_bbox(mapview.get_bbox())
+
         with wid.canvas:
-        # geom = gdf.loc[0, 'geometry']
-            for polygon in gdf['geometry']:
-                self.mesh = self.build_mesh(wid, polygon.exterior.coords)
+            for polygon in gis.gdf['geometry']:
+                self.mesh = self.build_mesh(polygon.exterior.coords)
                 self.mesh.mode = 'line_strip' # 'points', 'line_strip', 'line_loop', 'lines', 'triangle_strip', 'triangle_fan'
 
         layout = BoxLayout(size_hint=(1, None), height=50)
@@ -70,9 +100,15 @@ class MapViewApp(App):
         # root = BoxLayout(orientation='vertical')
         mapview.add_widget(wid)
         mapview.add_widget(layout)
+        # print(mapview.get_bbox())
+
+        mapview.on_touch_down(
+            # print(Window.mouse_pos)
+            # print(mapview.get_latlon_at(Window.mouse_pos))
+        )
 
         return mapview
 
 if __name__ == '__main__':
-    roi = ROI()
+    gis = GIS()
     MapViewApp().run()
