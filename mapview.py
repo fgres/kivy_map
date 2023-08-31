d9900bd76cadebcfe59233efb9fcf4074b7480a2
dunland <david.unland@web.de>
2023-08-31 14:10:49 +0200

    minor refactor of ROI/GIS class

--- mapview.py
+++ mapview.py
@@ -23,8 +23,11 @@ from kivy.graphics import Color
 
 
 
-class ROI:
-    def __init__(self):
+class GIS:
+    def __init__(self, shp):
+
+        self.gdf = geopandas.read_file(shp)#.to_crs(epsg=3857)
+
         self.lat_max = 54.1962362 # 7207409  # northernmost latitude
         self.lat_min = 54.1884733 # 7205932  # southernmost latitude
         self.lat_diff = self.lat_max - self.lat_min
@@ -32,26 +35,23 @@ class ROI:
         self.lon_min = 9.0995835 # 1012961  # westernmost longitude
         self.lon_diff = self.lon_max - self.lon_min
 
-class MapViewApp(App):
-
-    lat_max = 54.1962362 # 7207409  # northernmost latitude
-    lat_min = 54.1884733 # 7205932  # southernmost latitude
-    lat_diff = lat_max - lat_min
-    lon_max = 9.1091775 # 1014029  # easternmost longitude
-    lon_min = 9.0995835 # 1012961  # westernmost longitude
-    lon_diff = lon_max - lon_min
-
     def coords_to_point(self, coord, wid):
         target_size = 100
-        #longtitude 9 something
-        # x = (coord[0] - self.lon_min) * wid.size[0] / self.lon_diff
         x = (coord[0] - self.lon_min) * target_size / self.lon_diff + (wid.size[0] - target_size)/2
-        
-        #latitude 54 something
-        # y = (coord[1] - self.lat_min) * wid.size[1] / self.lat_diff
+
         y = (coord[1] - self.lat_min) * target_size / self.lat_diff + (wid.size[1] - target_size)/2
-        
+
         return x, y
+
+    # def set_bbox(self, coords):
+
+    #     self.lat_min = coords[0]
+    #     self.lon_min = coords[1]
+    #     self.lat_max = coords[2]
+    #     self.lon_max = coords[3]
+
+    #     print("updated GIS bounding box with coords:", self.coords)
+class MapViewApp(App):
 
     def build_mesh(self, wid, coords):
         vertices = []
@@ -59,19 +59,23 @@ class MapViewApp(App):
         indices = []
 
         for i, coord in enumerate(coords):
-            x, y = self.coords_to_point(coord, wid)
+            x, y = gis.coords_to_point(coord, wid)
             vertices.extend([x, y, 0, 0])
             indices.append(i)
         return Mesh(vertices=vertices, indices=indices)
-        
+
     def build(self):
         mapview = MapView(zoom=17, lat=54.19216979440788, lon=9.105268718909326)
-        self.lat_min, self.lon_min, self.lat_max, self.lon_max = mapview.get_bbox()
-        self.lat_diff = self.lat_max - self.lat_min
-        self.lon_diff = self.lon_max - self.lon_min
+
+        print(mapview.get_bbox())
+
+        gis.lat_min, gis.lon_min, gis.lat_max, gis.lon_max = mapview.get_bbox()
+        gis.lat_diff = gis.lat_max - gis.lat_min
+        gis.lon_diff = gis.lon_max - gis.lon_min
+
         m0 = MapMarker(lat=54.19216979440788, lon=9.105268718909326)  # Lille
-        m1 = MapMarker(lat=self.lat_min, lon=self.lon_min)  
-        m2 = MapMarker(lat=self.lat_max, lon=self.lon_max)  
+        m1 = MapMarker(lat=gis.lat_min, lon=gis.lon_min)
+        m2 = MapMarker(lat=gis.lat_max, lon=gis.lon_max)
         mapview.add_marker(m0)
         mapview.add_marker(m1)
         mapview.add_marker(m2)
@@ -80,10 +84,10 @@ class MapViewApp(App):
         wid = Widget(pos=(0, 0), size=Window.size)
         wid.canvas.add(Color(0, 0, 0))
         # print("==== widget size", wid.size)
-        gdf = geopandas.read_file("bestandsgebaeude_export.shp")
+
         with wid.canvas:
         # geom = gdf.loc[0, 'geometry']
-            for polygon in gdf['geometry']:
+            for polygon in gis.gdf['geometry']:
                 self.mesh = self.build_mesh(wid, polygon.exterior.coords)
                 self.mesh.mode = 'line_strip' # 'points', 'line_strip', 'line_loop', 'lines', 'triangle_strip', 'triangle_fan'
 
@@ -101,6 +105,5 @@ class MapViewApp(App):
         return mapview
 
 if __name__ == '__main__':
-    # roi = ROI()
+    gis = GIS("/home/dunland/github/qScope/data/GIS/Shapefiles/bestandsgebaeude_export.shp")
     MapViewApp().run()
-    
